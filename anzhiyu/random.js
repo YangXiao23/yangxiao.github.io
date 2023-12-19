2f6308b94140efd5a6540e54c5fbd4c7444cee16
yangxiao <a165973472@gmail.com>
2023-12-19 21:19:57 +0800

    Site updated: 2023-12-19 21:19:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/18/hello-world/"];function toRandomPost(){
+var posts=["2023/12/18/hello-world/","2023/12/19/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
