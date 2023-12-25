b2c341f72bb81178993904223915fb01fd8b93f2
yangxiao <a165973472@gmail.com>
2023-12-26 00:04:35 +0800

    Site updated: 2023-12-26 00:04:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/18/hello-world/","2023/12/19/test/"];function toRandomPost(){
+var posts=["2023/12/26/ElasticSearch/","2023/12/20/Mysql/","2023/12/18/hello-world/","2023/12/20/kafka/","2023/12/19/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
